86957e620d5a8f95806426af5e6e1f57580754d8
Luis Rosas <luisrosasorellano@gmail.com>
2018-08-19 19:35:37 -0500

    RestServer - Usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,12 @@
  * puerto
  */
 process.env.PORT = process.env.PORT || 3000;
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+let urlDB = `mongodb://${process.env.USERNAMEDB}:${process.env.PASSWORDDB}@ds125932.mlab.com:25932/cafe-luis`;
+
+if (process.env.NODE_ENV === 'local') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,19 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario');
-});
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-    res.json(body);
-});
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({id});
-});
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario');
-});
+app.use(require('./routes/usuarios'));
 
-app.listen(process.env.PORT, () => console.log(`Escuchando por el puerto ${process.env.PORT}`));
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE!');
+});
+app.listen(
+    process.env.PORT,
+    () => console.log(`Escuchando por el puerto ${process.env.PORT}`)
+);
